752c808e8b8657d71ac50ccd7b66f24c2a3ac1c0
안지애 <rozioh@naver.com>
2024-06-20 10:19:17 +0900

    Lombok 추가

--- SRC/swingguiAHN/src/main/java/board/db/MemberBean.java
+++ SRC/swingguiAHN/src/main/java/board/db/MemberBean.java
@@ -1,5 +1,8 @@
 package board.db;
 
+import lombok.Data;
+
+@Data
 public class MemberBean {
 	private String memberNo;
 	private String id;
@@ -18,68 +21,6 @@ public class MemberBean {
 			email + "\t" + birthdate + "\t" + hp + "\t" + addr + "\t" + 
 			lastLoginDt + "\t" + regDt
 		);
-	}
-	
-	// get, set
-	public String getMemberNo() {
-		return memberNo;
-	}
-	public void setMemberNo(String memberNo) {
-		this.memberNo = memberNo;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPw() {
-		return pw;
-	}
-	public void setPw(String pw) {
-		this.pw = pw;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getBirthdate() {
-		return birthdate;
-	}
-	public void setBirthdate(String birthdate) {
-		this.birthdate = birthdate;
-	}
-	public String getHp() {
-		return hp;
-	}
-	public void setHp(String hp) {
-		this.hp = hp;
-	}
-	public String getAddr() {
-		return addr;
-	}
-	public void setAddr(String addr) {
-		this.addr = addr;
-	}
-	public String getLastLoginDt() {
-		return lastLoginDt;
-	}
-	public void setLastLoginDt(String lastLoginDt) {
-		this.lastLoginDt = lastLoginDt;
-	}
-	public String getRegDt() {
-		return regDt;
-	}
-	public void setRegDt(String regDt) {
-		this.regDt = regDt;
 	}
 
 }
