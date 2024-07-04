a50552307b6941a5ef5445f3aaef98ab37f9fe00
안지애 <rozioh@naver.com>
2024-07-04 19:14:36 +0900

    Maven Project 생성

---
+++ SRC/swing-project/src/main/java/swing_ahn_work1/swing_project/App.java
@@ -0,0 +1,8 @@
+package swing_ahn_work1.swing_project;
+
+public class App {
+    
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ SRC/swing-project/src/test/java/swing_ahn_work1/swing_project/AppTest.java
@@ -0,0 +1,13 @@
+package swing_ahn_work1.swing_project;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
