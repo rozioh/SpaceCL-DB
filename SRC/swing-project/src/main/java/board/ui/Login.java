3e53ed56e630804706532e19b5fcbc9e9fa78310
안지애 <rozioh@naver.com>
2024-07-08 13:57:33 +0900

    feat: 로그인 버튼 클릭 및 Enter 키 입력으로 로그인 기능 구현
    - 패스워드 필드 JTextField에서 JPasswordField로 변경

--- SRC/swing-project/src/main/java/board/ui/Login.java
+++ SRC/swing-project/src/main/java/board/ui/Login.java
@@ -1,22 +1,28 @@
 package board.ui;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
+
+import board.db.MemberCRUD;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtId;
-	private JTextField txtPw;
+	private JPasswordField txtPw;
+	private MemberCRUD mMemberCRUD = new MemberCRUD();
 
 	/**
 	 * Launch the application.
@@ -60,6 +66,7 @@ public class Login extends JFrame {
 		panel.add(lblPw);
 		
 		txtPw = new JPasswordField();
+		txtPw.addActionListener(clickLogin); // 엔터 시 로그인
 		panel.add(txtPw);
 		txtPw.setColumns(10);
 		
@@ -67,7 +74,27 @@ public class Login extends JFrame {
 		contentPane.add(panelBtn);
 		
 		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(clickLogin);
 		panelBtn.add(btnLogin);
 	}
+	
+	private ActionListener clickLogin = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String id = txtId.getText();
+			String pw = new String(txtPw.getPassword());
+			
+			int cnt = mMemberCRUD.getFindMember(id, pw);
+			if(cnt > 0) {
+				JOptionPane.showMessageDialog(rootPane, "로그인 성공");
+				dispose(); // 로그인 창 닫기
+			}else {
+				JOptionPane.showMessageDialog(rootPane, "로그인 실패");
+			}
+			
+			
+		}
+	};
 
 }
